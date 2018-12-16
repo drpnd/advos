7d19e6aadf0c50b68d51afb6ad1638f5421c9df4
Hirochika Asai <panda@jar.jp>
2018-12-17 00:47:39 +0900

    Forgot to add arch.h....

---
+++ src/kernel/arch/x86_64/arch.h
@@ -0,0 +1,68 @@
+/*_
+ * Copyright (c) 2018 Hirochika Asai <asai@jar.jp>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _ADVOS_KERNEL_ARCH_H
+#define _ADVOS_KERNEL_ARCH_H
+
+#include <stdint.h>
+#include "const.h"
+
+#define sfence()        __asm__ __volatile__ ("sfence")
+
+void sti(void);
+void cli(void);
+uint64_t cpuid(uint64_t, uint64_t *, uint64_t *, uint64_t *);
+uint64_t rdtsc(void);
+uint64_t rdmsr(uint64_t);
+void wrmsr(uint64_t, uint64_t);
+uint32_t mfrd32(uintptr_t);
+void mfwr32(uintptr_t, uint32_t);
+uint8_t inb(uint16_t);
+uint16_t inw(uint16_t);
+uint32_t inl(uint16_t);
+void outb(uint16_t, uint8_t);
+void outw(uint16_t, uint16_t);
+void outl(uint16_t, uint32_t);
+void lgdt(void *, uint64_t);
+void sgdt(void *);
+void lidt(void *);
+void sidt(void *);
+void lldt(uint16_t);
+void hlt(void);
+void pause(void);
+
+/* Interrupt handlers */
+void intr_null(void);
+void intr_irq1(void);
+void intr_crash(void);
+
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
