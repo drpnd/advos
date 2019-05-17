2fbafd8f016222ec4c5958b0e90c91432d52e7c3
Hirochika Asai <panda@jar.jp>
2019-05-17 22:29:59 +0900

    Move tsc-related functions to tsc.c.

--- src/kernel/arch/x86_64/arch.c
+++ src/kernel/arch/x86_64/arch.c
@@ -54,60 +54,6 @@ typedef struct {
     uint32_t attr;
 } __attribute__ ((packed)) sysaddrmap_entry_t;
 
-
-/*
- * invariant_tsc_freq -- resolve the base frequency of invariant TSC
- */
-uint64_t
-invariant_tsc_freq(void)
-{
-    uint64_t reg;
-    uint64_t rax;
-    uint64_t rbx;
-    uint64_t rcx;
-    uint64_t rdx;
-    uint64_t family;
-    uint64_t model;
-
-    /* Check Invariant TSC support */
-    cpuid(0x80000007, &rbx, &rcx, &rdx);
-    if ( !(rdx & 0x100) ) {
-        return 0;
-    }
-
-    /* Read TSC frequency */
-    reg = (rdmsr(MSR_PLATFORM_INFO) & 0xff00) >> 8;
-
-    /* CPUID;EAX=0x01 */
-    rax = cpuid(0x01, &rbx, &rcx, &rdx);
-    family = ((rax & 0xf00) >> 8) | ((rax & 0xff00000) >> 12);
-    model = ((rax & 0xf0) >> 4) | ((rax & 0xf0000) >> 12);
-    if ( 0x06 == family ) {
-        switch ( model ) {
-        case 0x1e: /* Nehalem */
-        case 0x1a: /* Nehalem */
-        case 0x2e: /* Nehalem */
-            /* 133.33 MHz */
-            return reg * 133330000;
-        case 0x2a: /* SandyBridge */
-        case 0x2d: /* SandyBridge-E */
-        case 0x3a: /* IvyBridge */
-        case 0x3c: /* Haswell */
-        case 0x3d: /* Broadwell */
-        case 0x46: /* Skylake */
-        case 0x4e: /* Skylake */
-        case 0x57: /* Xeon Phi */
-            /* 100.00 MHz */
-            return reg * 100000000;
-        default:
-            /* Presumedly 100.00 MHz for other processors */
-            return reg * 100000000;
-        }
-    }
-
-    return 0;
-}
-
 /*
  * Convert a hexdecimal 4-bit value to an ascii code
  */
