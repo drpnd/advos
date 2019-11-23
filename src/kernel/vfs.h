082b3b085bcc8efc13460d5a1152e7b7051f07c6
Hirochika Asai <panda@jar.jp>
2019-11-23 08:32:20 +0800

    Add the interfaces for vfs.

--- src/kernel/vfs.h
+++ src/kernel/vfs.h
@@ -69,6 +69,12 @@ typedef struct {
     vfs_vnode_t * (*readlink)(vfs_mount_t *, vfs_vnode_t *);
     vfs_vnode_t * (*mmap)(vfs_mount_t *, vfs_vnode_t *);
     vfs_vnode_t * (*close)(vfs_mount_t *, vfs_vnode_t *);
+    /* Process control (+advlock) */
+    int * (*ioctl)(vfs_mount_t *, vfs_vnode_t *, int, void *);
+    int * (*poll)(vfs_mount_t *, vfs_vnode_t *);
+    /* Object management (+inactive, reclaim) */
+    int * (*lock)(vfs_mount_t *, vfs_vnode_t *);
+    int * (*unlock)(vfs_mount_t *, vfs_vnode_t *);
 } vfs_interfaces_t;
 
 /*
